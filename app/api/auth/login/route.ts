9b069c6e31c7e04b23812b00eca6911b71ca7b71
Johannes <98045373+Luxcorel@users.noreply.github.com>
2023-12-09 00:31:37 +0100

    Added preliminary backend boilerplate

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,11 @@
+export async function GET(request: Request) {}
+
+export async function HEAD(request: Request) {}
+
+export async function POST(request: Request) {}
+
+export async function PUT(request: Request) {}
+
+export async function DELETE(request: Request) {}
+
+export async function PATCH(request: Request) {}
