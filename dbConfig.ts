cfbcdc5ef0601169764d063163aaad670003c0cc
Johannes <98045373+Luxcorel@users.noreply.github.com>
2023-12-10 02:17:51 +0100

    Now using kysely-codegen to autogenerate db types

--- dbConfig.ts
+++ dbConfig.ts
@@ -1,39 +1,4 @@
-import type { GeneratedAlways } from "kysely";
 import { createKysely } from "@vercel/postgres-kysely";
+import type { DB } from "kysely-codegen";
 
-interface Database {
-  User: {
-    id: GeneratedAlways<string>;
-    name: string | null;
-    email: string;
-    emailVerified: Date | null;
-    image: string | null;
-  };
-  Account: {
-    id: GeneratedAlways<string>;
-    userId: string;
-    type: string;
-    provider: string;
-    providerAccountId: string;
-    refresh_token: string | null;
-    access_token: string | null;
-    expires_at: number | null;
-    token_type: string | null;
-    scope: string | null;
-    id_token: string | null;
-    session_state: string | null;
-  };
-  Session: {
-    id: GeneratedAlways<string>;
-    userId: string;
-    sessionToken: string;
-    expires: Date;
-  };
-  VerificationToken: {
-    identifier: string;
-    token: string;
-    expires: Date;
-  };
-}
-
-export const db = createKysely<Database>();
+export const db = createKysely<DB>();
