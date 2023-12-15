16aa63e1581f4b0d9ff037e81c8ea75a6fe2755f
Johannes <98045373+Luxcorel@users.noreply.github.com>
2023-12-15 03:13:22 +0100

    Small changes to project structure

--- app/api/suggestions/weather/route.ts
+++ app/api/suggestions/weather/route.ts
@@ -1,11 +1,1 @@
 export async function GET(request: Request) {}
-
-export async function HEAD(request: Request) {}
-
-export async function POST(request: Request) {}
-
-export async function PUT(request: Request) {}
-
-export async function DELETE(request: Request) {}
-
-export async function PATCH(request: Request) {}
