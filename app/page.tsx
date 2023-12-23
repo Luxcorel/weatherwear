c7a3a383d9484ba76f942d6ff0efd4a1f3399bf9
Victor Pirojoc <120030846+Senpai-paj@users.noreply.github.com>
2023-12-23 10:40:35 +0100

    background color deleted

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <div className={"bg-red-400"}>
+        <div className={""}>
             <h1 className={"my-5 text-center text-2xl"}>Welcome to Weather-wear</h1>
         </div>
     );
