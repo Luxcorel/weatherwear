cb49d8808d1814ae54b6fa0d998ef01caeb4dea9
Johannes <98045373+Luxcorel@users.noreply.github.com>
2024-01-01 00:10:46 +0100

    Added geocoding api call

--- lib/weather-api-requests.ts
+++ lib/weather-api-requests.ts
@@ -1,4 +1,5 @@
-export const BASE_URL_WEATHER_API = `https://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}`;
+const BASE_URL_WEATHER_API_CURRENT_WEATHER = `https://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}`;
+const BASE_URL_WEATHER_API_LOCATION = `https://api.weatherapi.com/v1/search.json?key=${process.env.WEATHER_API_KEY}`;
 
 /**
  * Returns fetch() Promise
@@ -6,7 +7,28 @@ export const BASE_URL_WEATHER_API = `https://api.weatherapi.com/v1/current.json?
  * @param longitude longitude of location
  */
 export function fetchWeatherByLocation(latitude: number, longitude: number) {
-  return fetch(BASE_URL_WEATHER_API + `&q=${latitude}, ${longitude}`, {
+  return fetch(BASE_URL_WEATHER_API_CURRENT_WEATHER + `&q=${latitude}, ${longitude}`, {
     next: { revalidate: 60 },
   });
 }
+
+export function searchLocationByName(query: string) {
+  return fetch(BASE_URL_WEATHER_API_LOCATION + `&q=${query}`, {
+    next: { revalidate: 60 },
+  });
+}
+
+export type LocationAPIResponse = {
+  id: number;
+  name: string;
+  region: string;
+  country: string;
+  lat: number;
+  lon: number;
+  url: string;
+};
+
+export type LocationAPIResponseFailure = {
+  code: number;
+  message: string;
+};
