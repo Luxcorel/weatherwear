3daa62264cf8f4dfc41885e137660cf30fd1b16f
Johannes <98045373+Luxcorel@users.noreply.github.com>
2024-01-02 00:52:54 +0100

    Update outfit-response.ts

--- frontend-types/outfit-response.ts
+++ frontend-types/outfit-response.ts
@@ -1,9 +1,1 @@
-import { ClothingDTO } from "@/frontend-types/clothing-dto";
-
 export type WeatherKeyword = "sunny" | "cloudy" | "rainy" | "snowy";
-
-export interface OutfitResponse {
-  outfit: ClothingDTO[];
-  weather_keyword: WeatherKeyword;
-  weather_picture: string;
-}
