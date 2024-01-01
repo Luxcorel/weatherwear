b3ed4f0135ed15e48a249dad9e563471badb78c6
Johannes <98045373+Luxcorel@users.noreply.github.com>
2024-01-01 02:00:43 +0100

    Added "search for new location" component

--- components/locations/NewLocation.tsx
+++ components/locations/NewLocation.tsx
@@ -1,3 +1,60 @@
+"use client";
+
+import { ChangeEvent, FormEvent, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { GeocodingResponse } from "@/lib/weather-api-requests";
+
 export default function NewLocation() {
-    return <></>;
+    const [locationQuery, setLocationQuery] = useState("");
+    const [fetchedLocation, setFetchedLocation] = useState<GeocodingResponse>();
+
+    const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setLocationQuery(event.target.value);
+    };
+
+    const handleSearchSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const location = (await fetch(`api/geocoding/${locationQuery}`).then(async (r) => r.json())) as
+            | GeocodingResponse
+            | undefined;
+
+        if (location) {
+            setFetchedLocation(location);
+        }
+    };
+
+    const handleLocationSelect = async () => {
+        const response = await fetch("api/locations", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                location_name: fetchedLocation?.name,
+                latitude: fetchedLocation?.lat,
+                longitude: fetchedLocation?.lon,
+            }),
+        });
+
+        window.location.reload();
+    };
+
+    return (
+        <div className={"flex-wrap"}>
+            <form onSubmit={handleSearchSubmit}>
+                <input
+                    className="rounded-md border px-4 py-2 focus:outline-none"
+                    type="text"
+                    placeholder="Add a new location"
+                    value={locationQuery}
+                    onChange={handleSearchChange}
+                />
+                <Button type="submit">Search</Button>
+            </form>
+            {fetchedLocation?.name ? (
+                <Button onClick={handleLocationSelect}>{`Add ${fetchedLocation.name}`}</Button>
+            ) : null}
+        </div>
+    );
 }
