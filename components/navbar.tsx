7b8babf11c7706dd9136f845d09369dd007ef9a2
Johannes <98045373+Luxcorel@users.noreply.github.com>
2024-01-02 17:25:37 +0100

    Added wardrobe navbar entry

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -28,7 +28,19 @@ export default async function Navbar() {
                     </Button>
                 </Link>
             </div>
-            {/*  
+
+            <div>
+                <Link href={"/wardrobe"}>
+                    <Button
+                        className={
+                            "rounded-3xl border-2 border-gray-500 bg-blue-100 text-lg text-black duration-500 ease-in-out hover:border-green-500 hover:bg-white dark:bg-black dark:text-white"
+                        }
+                    >
+                        Wardrobe
+                    </Button>
+                </Link>
+            </div>
+            {/*
             <div>
                 <Link href={"/setup"}>
                     <Button
