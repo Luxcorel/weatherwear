dc6878d31d02c77ad47a22983d699426a3371a9a
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2026-03-20 01:43:40 +0100

    fix: update eslint config to new format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,60 +1,16 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all,
-});
-
-export default defineConfig([
-    globalIgnores([
-        "components/ui",
-        "node_modules",
-        ".pnp",
-        "**/.pnp.js",
-        "coverage",
-        ".next/",
-        "out/",
-        "build",
-        "**/.DS_Store",
-        "**/*.pem",
-        "**/npm-debug.log*",
-        "**/yarn-debug.log*",
-        "**/yarn-error.log*",
-        "**/.pnpm-debug.log*",
-        "**/.env*.local",
-        "**/.vercel",
-        "**/*.tsbuildinfo",
-        "**/next-env.d.ts",
-        "**/node_modules",
-        "**/public",
-        "**/build",
-        "**/dist",
-        "**/.cache",
-        "**/package-lock.json",
-        "**/next.config.ts",
-    ]),
-    ...compat.extends("xo"),
-    ...compat.extends("plugin:tailwindcss/recommended"),
-    ...nextCoreWebVitals,
-    ...compat.extends("prettier"),
-    {
-        rules: {
-            "react/jsx-curly-brace-presence": "off",
-            "react/jsx-no-useless-fragment": "off",
-            "new-cap": "off",
-            camelcase: "off",
-            "no-warning-comments": "off",
-            "capitalized-comments": "off",
-            "spaced-comment": "off",
-            "no-unused-vars": "off",
-        },
-    },
-]);
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+ 
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+])
+ 
+export default eslintConfig
